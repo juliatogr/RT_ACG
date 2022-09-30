57e709d57ba1eff771d02b5627f781696c512b31
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-09-30 16:32:24 +0200

    task3 structure

--- src/shaders/depthshader.cpp
+++ src/shaders/depthshader.cpp
@@ -1,17 +1,12 @@
 #include "depthshader.h"
 #include "../core/utils.h"
 
-DepthShader::DepthShader()
-    
-{ 
-    //hitColor(Vector3D(1, 0, 0))
-}
+DepthShader::DepthShader() : color(Vector3D(1, 0, 0))  
+{ }
 
-DepthShader::DepthShader(Vector3D color_, double maxDist_, Vector3D bgColor_)
-   
-{ 
-    // Shader(bgColor_), hitColor(hitColor_)
-}
+DepthShader::DepthShader(Vector3D color_, double maxDist_, Vector3D bgColor_) :
+    Shader(bgColor_), color(color_), maxDist(maxDist_)
+{ }
 
 Vector3D DepthShader::computeColor(const Ray &r, const std::vector<Shape*> &objList, const std::vector<PointLightSource> &lsList) const
 {
